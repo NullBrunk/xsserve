fe2b780475dd15affd6dde42ed67c61ddaa801aa
Anas <125673909+NullBrunk@users.noreply.github.com>
2025-01-01 15:31:42 +0100

    Delete script.py

--- script.py
+++
@@ -1,8 +0,0 @@
-import netifaces
-
-def filter_function(value):
-	return not value.startswith("br-") and not value.startswith("dock
-er") and not value == "lo"
-
-for inet in filter(filter_function, netifaces.interfaces()):
-	iface = netifaces.ifaddresses("wlo1")[netifaces.AF_INET][0]["addr"]
